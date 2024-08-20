4c0ad54ff5feb05446a415843559a59089b96891
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-08-20 09:59:00 +0530

    Using Express Router and Adding 404 page

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.json());
 
-app.use('/add-product', (req, res, next) => {
-  console.log('Product page');
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello From Express</h1>');
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000, () => {
