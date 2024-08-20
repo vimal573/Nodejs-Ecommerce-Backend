5d54a89c68307fc2b099dd8592621bd8479543a6
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-08-20 09:19:03 +0530

    Parsing Incoming Requests

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log('This always runs');
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/add-product', (req, res, next) => {
   console.log('Product page');
-  res.send('<h1>The add product page</h1>');
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('In another middleware');
   res.send('<h1>Hello From Express</h1>');
 });
 
