a2a6c12bacdf13327d7229ee2b82d7eb25d44dfc
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-08-20 08:33:43 +0530

    Adding Middleware

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const http = require('http');
 
-const server = http.createServer();
+const express = require('express');
 
-server.listen(3000);
+const app = express();
+
+const server = http.createServer(app);
+
+app.use((req, res, next) => {
+  console.log('In the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>Hello From Express</h1>');
+});
+
+server.listen(3000, () => {
+  console.log('server started');
+});
