38f839295c256c86256ddabd9215eebbe1810995
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-08-21 11:58:15 +0530

    Implementing ejs

--- routes/admin.js
+++ routes/admin.js
@@ -11,9 +11,12 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
   res.render('add-product', {
     pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
   });
 });
 
@@ -25,6 +28,5 @@ router.post('/add-product', (req, res, next) => {
   res.redirect('/');
 });
 
-// module.exports = router;
 exports.routes = router;
 exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -9,12 +9,14 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 
   res.render('shop', {
     prods: products,
     pageTitle: 'shop',
     path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
   });
 });
 
