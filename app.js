945792c7d571d8716a5c80f24a8524dc75893eaa
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-08-22 10:04:09 +0530

    Finishing Controllers

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -18,9 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page not found', path: '/' });
-});
+app.use(errorController.get404);
 
 app.listen(3000, () => {
   console.log('server started');
