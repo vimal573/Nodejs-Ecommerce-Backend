44da9f9cb5bc89e2238f71bc269a2b07667227e8
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-09-03 22:52:40 +0530

    Pre-Populating the Edit Product Page with Data

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,6 +4,7 @@ exports.getAddProducts = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
+    editing: false,
   });
 };
 
@@ -19,10 +20,19 @@ exports.getEditProducts = (req, res, next) => {
   if (!editMode) {
     return req.redirect('/');
   }
-  res.render('admin/edit-product', {
-    pageTitle: 'Edit Product',
-    path: '/admin/edit-product',
-    editing: editMode,
+  const prodId = req.params.productId;
+
+  Product.findById(prodId, product => {
+    console.log(product);
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product,
+    });
   });
 };
 
