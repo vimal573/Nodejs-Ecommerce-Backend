57b01cfb5c85a81414db7c631127c30c44a8ad6e
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-09-05 20:09:07 +0530

    Working on the Shopping Cart

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
   name: {
     type: String,
     required: true,
@@ -12,18 +14,38 @@ const userSchema = new mongoose.Schema({
   cart: {
     items: [
       {
-        productID: {
-          type: mongoose.Schema.Types.ObjectId,
+        productId: {
+          type: Schema.Types.ObjectId,
           ref: 'Product',
           required: true,
         },
-        quantity: {
-          type: Number,
-          required: true,
-        },
+        quantity: { type: Number, required: true },
       },
     ],
   },
 });
+
+userSchema.methods.addToCart = function (product) {
+  const cartProductIndex = this.cart.items.findIndex(cp => {
+    return cp.productId.toString() === product._id.toString();
+  });
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity,
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems,
+  };
+  this.cart = updatedCart;
+  return this.save();
+};
 
 module.exports = mongoose.model('User', userSchema);
