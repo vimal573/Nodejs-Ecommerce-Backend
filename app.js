bd767f3d65ddee3fe430085e7dfb212395710577
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-09-06 22:13:46 +0530

    Using MongoDB toStore Sessions

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+  'mongodb+srv://vimal2540vk:exvsyLmtz06A5zRz@cluster0.kjiau.mongodb.net/shop';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -24,6 +32,7 @@ app.use(
     secret: 'my secret',
     resave: false,
     saveUninitialized: false,
+    store: store,
   })
 );
 
@@ -43,13 +52,10 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://vimal2540vk:exvsyLmtz06A5zRz@cluster0.kjiau.mongodb.net/shop',
-    {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    }
-  )
+  .connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
