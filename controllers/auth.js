bb60bac08b37223b738a0cea76caa5a22d906493
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-09-07 17:04:06 +0530

    Finished All flash messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,17 +2,30 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    errorMessage: req.flash('error'),
+    errorMessage: message,
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
+    errorMessage: message,
   });
 };
 
@@ -36,6 +49,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect('/');
             });
           }
+          req.flash('error', 'Invalid email or password');
           res.redirect('login');
         })
         .catch(err => {
@@ -52,6 +66,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email })
     .then(userDoc => {
       if (userDoc) {
+        req.flash('error', 'Email exist already, please pick diffrent one');
         return res.redirect('/signup');
       }
       return bcrypt
