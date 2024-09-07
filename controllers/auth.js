a0aa90e797f8a35f3dd85fe6f50fae6ab48670f8
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-09-07 15:22:23 +0530

    Sign Up view and Routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,14 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false,
+  });
+};
+
 exports.postLogin = (req, res, next) => {
   User.findById('66d9d87a784c4a1fdc4b72d9')
     .then(user => {
@@ -20,6 +28,8 @@ exports.postLogin = (req, res, next) => {
     })
     .catch(err => console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
