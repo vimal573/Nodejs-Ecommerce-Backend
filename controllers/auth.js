fe7007a0f1b5c3315e4145b08ac75c0670aeed18
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-09-15 13:20:51 +0530

    implementing and creating Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodeMailer = require('nodemailer');
 
@@ -126,5 +127,40 @@ exports.getReset = (req, res, next) => {
     path: '/reset',
     pageTitle: 'Reset Password',
     errorMessage: message,
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          req.flash('error', 'No account with that email found.');
+          return res.redirect('/reset');
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then(result => {
+        res.redirect('/');
+        transport.sendMail({
+          to: req.body.email,
+          from: 'shop@node-complete.com',
+          subject: 'Password reset',
+          html: `
+          <p>You requested a password reset</p>
+          <p>Click this <a href="http://localhost:300/reset/${token}">link</a> to set a new password.</p>
+          `,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   });
 };
